79abb8fc677c603a6102f9d420b01df36bdba51a
Bamiwo Adebayo <bammietop@gmail.com>
2024-04-09 16:58:02 +0100

    ES6 Basics

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -7,7 +7,5 @@ export default function getFullBudgetObject(income, gdp, capita) {
     getIncomeInDollars: (income) => `$${income}`,
     getIncomeInEuros: (income) => `${income} euros`,
   };
-
   return fullBudget;
 }
-
