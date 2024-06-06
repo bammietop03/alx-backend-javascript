9b8aa7ded8fe46311aa066449a8d170035443494
Bamiwo Adebayo <bammietop@gmail.com>
2024-06-06 18:13:13 +0100

    unittests in js

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,38 @@
+// 0-calcul.test.js
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', function() {
+  it('should return 4 when inputs are 1 and 3', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5 when inputs are 1 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 when inputs are 1.2 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 when inputs are 1.5 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  // Additional edge cases
+  it('should return 0 when inputs are 0 and 0', function() {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+
+  it('should return -2 when inputs are -1 and -1', function() {
+    assert.strictEqual(calculateNumber(-1, -1), -2);
+  });
+
+  it('should return 2 when inputs are -1.4 and 3.4', function() {
+    assert.strictEqual(calculateNumber(-1.4, 3.4), 2);
+  });
+
+  it('should return 3 when inputs are -1.4 and 4.4', function() {
+    assert.strictEqual(calculateNumber(-1.4, 4.4), 3);
+  });
+});
